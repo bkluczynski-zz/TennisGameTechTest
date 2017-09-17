042a1e9b997ffbf6dca725c21fbb60ec2f2704c6
Bart <kluczynskib@gmail.com>
2017-09-17 20:07:49 +0100

    added express server to the app on the server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,10 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+// server/index.js
+'use strict';
 
-const app = express();
+const app = require('./app');
 
-// Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+const PORT = process.env.PORT || 9000;
 
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
-// Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
-
-module.exports = app;
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import Player from './Player'
 import { connect } from 'react-redux';
-import { Router, Route } from 'react-router';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Player/>
-      </div>
+      <Player/>
     );
   }
 }
